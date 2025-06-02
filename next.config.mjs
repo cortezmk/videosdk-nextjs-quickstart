d6c6bce6ab53859a573b8c68646952e4c5a17ee8
mkortas <mkortas@L57H0H9>
2025-06-02 11:27:22 +0200

    changed settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
--- src/components/settings.ts
+++ src/components/settings.ts
@@ -1,3 +1,4 @@
 export default {
-  serviceUrl: "https://32ab-83-21-91-40.ngrok-free.app"
+  // serviceUrl: "https://32ab-83-21-91-40.ngrok-free.app"
+  serviceUrl: ""
 };
